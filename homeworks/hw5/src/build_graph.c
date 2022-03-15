e6122d4c6dace0d5a564eddb894bef4d898019c8
Thomas Schollenberger <tom@schollenbergers.com>
2022-03-14 22:42:18 -0400

    fastforward hw5

--- homeworks/hw5/src/build_graph.c
+++ homeworks/hw5/src/build_graph.c
@@ -1,8 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "simple_graph.h"
+// #include "simple_graph.h"
 
 int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t lineSize = 0;
+
+    lineSize = getline(&line, &len, stdin);
+
+    printf("You entered %s, which has %zu chars.\n", line, lineSize - 1);
+
+    free(line);
 
     return EXIT_SUCCESS;
 }
